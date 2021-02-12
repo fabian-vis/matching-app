893a2dd7cd46231b1c3035e776c41fb5fb1b8ad8
fabian-vis <fabianvis988@gmail.com>
2021-02-12 11:39:44 +0100

    tussentijdse save

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("werkt dit");
+const express = require('express')
+const app = express()
+const port = 8000
+
+app.use(express.static('static'))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/test', (req, res) => {
+    res.send('testpagina')
+  })
+
+app.get('*', (req, res) => {
+  res.send('error 404');
+});
+
+app.listen(port, () => {
+  console.log(`Server opgestart at http://localhost:${port}`)
+})
